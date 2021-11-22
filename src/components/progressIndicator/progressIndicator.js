952e9497375b6b1151b3d2caffeff170d5a33ad1
Pitter Park <70798972+pitterpark@users.noreply.github.com>
2021-11-22 11:11:37 +0900

    Delete src/components directory

--- src/components/progressIndicator/progressIndicator.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./ProgressIndicator.css";
-
-export const ProgressIndicator = () => (
-  <div className="loading-container">
-    <div className="loading"></div>
-    <div id="loading-text">loading</div>
-  </div>
-);
